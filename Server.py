020b8b77d56f013bd1398df23fec19c5ace8f323
Harsh Pathak <harsh9607@gmail.com>
2017-10-08 16:37:20 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -12,19 +12,21 @@ s.bind((host,8888))
 
 # server ready to listen to 2 connections at max 
 s.listen(3)
- 
+print('Server:: waiting for Connections:') 
 c , clientaddr = s.accept()   
+print('Server::Got a connection from client side')
 while True:
-    print('Server:: waiting for Connections:')
-  
-  #c , clientaddr = s.accept()    #tuple
-    #(client,(ip,port))=sock.accept()
-    print('Server::Got a connection from client side')
-    c.send('You are Connected to Harsh101 server services')
+ 
     data = c.recv(2017)
     print(str(data))
-    c.send('Got your msg , Thanks for connecting ')
-    print('Served out client !')
     
-c.close()
+    if str(data) == 'bye' :
+    	break
+    msg2send = raw_input('Enter message for Client :: ')
+    c.send(msg2send.encode())
+    if str(msg2send) == 'bye' :
+    	break
+  
 
+print('closing socket !')    
+c.close()
