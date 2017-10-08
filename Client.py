e131c636e35752d22fe9d7edf0153ecc4563d3ad
Harsh Pathak <harsh9607@gmail.com>
2017-10-08 16:38:12 +0530

    Update Client.py

--- Client.py
+++ Client.py
@@ -7,15 +7,24 @@ s = socket.socket()
 host = socket.gethostname()
 port = 8888
 
-# msg to be sent to the user 
-#msg = "harsh pathak sending msg to server"
 
 s.connect((host,port))
 
 while True:
 	msg = raw_input('Enter your message :: ')
 	print('Client side running ! Sending msg')	
-	print(s.recv(2017))
+	#print(s.recv(2017))
 	s.send(msg.encode())
+	if msg == 'bye' :
+		break
+		
+	received_msg = s.recv(2017)
+	print(str(received_msg))
+	if str(received_msg) == 'bye':
+		break
+
 	
+	
+
+print('closing socket')	
 s.close()
